ca30a2ec2f2babda5a4245e778929611a48876c0
gupta-rajan <rjngupta22@gmail.com>
2024-06-17 12:32:08 +0530

    update apiUrl

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
+// api.js (located in the root directory)
 import axios from "axios";
 
-const apiUrl = "/choreo-apis/speechmodels/backend/v1";
+const apiUrl = "http://10.195.250.59:8000";
 
 const api = axios.create({
   baseURL: apiUrl,
@@ -10,13 +11,19 @@ export const uploadFile = async (file) => {
   const formData = new FormData();
   formData.append('audio', file);
 
-  const response = await api.post('/speech/upload/', formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  });
+  try {
+    const token = localStorage.getItem('accessToken'); // Adjust this based on how your token is stored
+    const response = await api.post('/speech/upload/', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'Authorization': `Bearer ${token}`, // Include the authorization header
+      },
+    });
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.data.message || 'Error uploading file');
+  }
 };
 
 export default api;
